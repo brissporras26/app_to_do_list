d80682d2970b8438788e7ebd23ec3d7990235fa6
brissa <porras.acc1@outlook.com>
2024-09-04 03:00:29 -0600

    create tasks

--- app/routes/home.py
+++ app/routes/home.py
@@ -1,13 +1,10 @@
-from flask import Blueprint, render_template, jsonify
-from app.logic.task_logic import get_task_by_name
+from flask import Blueprint, render_template
 from database.database_manager import database_manager
 
 home_bp = Blueprint('home', __name__)
 
 @home_bp.route('/')
 def home():
-    # Obtener la colección de tareas directamente desde la instancia de database_manager
-    tasks_collection = database_manager.get_db('ToDoCluster')['tasks']  # Asegúrate de reemplazar 'your_database_name' por tu base de datos real
-    tasks = list(tasks_collection.find())  # Obtener todas las tareas y convertir el cursor a una lista
-    
+    tasks_collection = database_manager.get_db()['tasks']
+    tasks = list(tasks_collection.find())
     return render_template('home.html', tasks=tasks)
