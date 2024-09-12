b8f92ee713d2df4cb99b5d1372c458e7def77d0d
brissa <porras.acc1@outlook.com>
2024-09-12 16:11:24 -0600

    create route index

--- app/__init__.py
+++ app/__init__.py
@@ -23,14 +23,14 @@ def create_app():
         from app.routes.home import home_bp
         from app.routes.user import user_bp
         from app.routes.tasks import task_bp
-        #from app.routes.index import index_bp
+        from app.routes.index import index_bp
         
 
         
         app.register_blueprint(home_bp)
         app.register_blueprint(user_bp)
         app.register_blueprint(task_bp)
-        #app.register_blueprint(index_bp)
+        app.register_blueprint(index_bp)
 
 
 
